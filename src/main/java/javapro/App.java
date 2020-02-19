186a11135a3700d8a17d2807867769ffbdf2636d
hari <hari123@gmail.com>
2020-02-19 20:15:20 +0530

    ss commit

---
+++ src/main/java/javapro/App.java
@@ -0,0 +1,13 @@
+package javapro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/javapro/hari.java
@@ -0,0 +1,10 @@
+package hari;
+
+import java.util.*;
+class hari
+{
+public static void main(String []args)
+{
+System.out.print("hiiiiiii");
+}
+}
